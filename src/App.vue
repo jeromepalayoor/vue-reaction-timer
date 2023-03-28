f5a3b5572ed8f0071b15f2536c968d5191a08107
jeromepalayoor <me@jp-ch.gq>
2023-03-29 00:56:44 +0800

    idkwhatimdoing

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <h1>Jerome's Reaction Timer Created With Vue.JS 3</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-
+import Block from './components/Block.vue'
+//import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: { }
+  components: {
+    Block
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    }
+  }
 }
 </script>
 
@@ -19,5 +36,12 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+
+button {
+  padding: 7px;
+  border: 2px solid darkgrey;
+  background-color: antiquewhite;
+  border-radius: 5px;
 }
 </style>
