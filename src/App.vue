324217770f0e2f5cdd14938ebe7cd819b833de25
jeromepalayoor <me@jp-ch.gq>
2023-03-29 01:23:25 +0800

    completed?

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <h1>Jerome's Reaction Timer Created With Vue.JS 3</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from './components/Block.vue'
-//import Results from './components/Results.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    Block
+    Block,
+    Results
   },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
@@ -40,6 +50,7 @@ export default {
 
 button {
   padding: 7px;
+  width: 400px;
   border: 2px solid darkgrey;
   background-color: antiquewhite;
   border-radius: 5px;
---
+++ src/components/Results.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="results">Score: {{ this.score }}ms</div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+};
+</script>
+
+<style>
+.results {
+    width: 400px;
+    border-radius: 20px;
+    background-color: gold;
+    color: black;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
